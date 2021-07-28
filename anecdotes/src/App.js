674670a459330ef8512ad1a0619d46d286f4f480
laptop <juha.jylhakoski@aalto.fi>
2021-07-29 06:47:59 +0800

    7.1

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
 
+import ReactDOM from 'react-dom'
+
+// 7.1 
+// $ npm install react-router-dom
+
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom"
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <h1>Software anecdotes</h1>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -84,6 +95,7 @@ const CreateNew = (props) => {
 }
 
 const App = () => {
+
   const [anecdotes, setAnecdotes] = useState([
     {
       content: 'If it hurts, do it more often',
@@ -123,14 +135,32 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Software anecdotes</h1>
+
+    <Router>
+
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
+
+      <Switch>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/create">
+          <CreateNew addNew={addNew} />
+        </Route>
+
+        <Route path="/">
+          <AnecdoteList anecdotes={anecdotes} />
+        </Route>
+
+      </Switch>
+
+      <div>
+        <Footer />
+      </div>
+
+    </Router>
   )
 }
 
