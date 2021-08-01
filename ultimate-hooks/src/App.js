88c6a57cde810407d79938385ea7cfadf1383f80
laptop <juha.jylhakoski@aalto.fi>
2021-08-01 16:20:19 +0800

    7.8

---
+++ ultimate-hooks/src/App.js
@@ -0,0 +1,125 @@
+  
+import React, { useState, useEffect } from 'react'
+
+// $ npm install axios --save
+
+import axios from 'axios'
+
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+// 7.8
+const useResource = (baseUrl) => {
+
+  console.log('baseUrl', baseUrl)
+
+  const [resources, setResources] = useState([])
+
+  const getAll = async () => {
+
+    const response = await axios.get(baseUrl)
+
+    console.log('response', response.data)
+
+    setResources(response.data)
+
+    return response.data
+  }
+
+  const create = async (resource) => {
+
+    console.log('resource', resource)
+
+    const response = await axios.post(baseUrl,resource)
+
+    console.log('response', response)
+
+    return response.data
+  }
+
+  const service = {
+    create,
+    getAll,
+  }
+
+  return [
+    resources, 
+    service
+  ]
+}
+
+const App = () => {
+
+  const content = useField('text')
+
+  const name = useField('text')
+
+  const number = useField('text')
+
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const [contents, contentsService] = useState('')
+
+  const [names, namesService] = useState('')
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    
+    noteService.create({ content: content.value })
+
+    contentsService(content.value)
+  }
+ 
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+
+    personService.create({ name: name.value, number: number.value})
+
+    namesService(name.value)
+  }
+
+  useEffect(() => {
+     noteService.getAll()
+     console.log('notes', notes)
+  }, [contents])
+
+  useEffect(() => {
+     personService.getAll()
+     console.log('persons', persons)
+  }, [names])
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
+}
+
+export default App
